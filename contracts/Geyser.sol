45a52a53c14f4159f6e02fbe1488316e50cd8c55
sfjbaihe <support@gnp.co>
2020-08-25 09:23:39 +0800

    rename stake to geyser

--- contracts/Geyser.sol
+++ contracts/Geyser.sol
@@ -2,7 +2,7 @@ pragma solidity ^0.4.26;
 
 import "./Stake.sol";
 
-contract GNPStake is Stake {
+contract Geyser is Stake {
     constructor(address _owner) public {
         Stake.initialize(_owner);
     }
